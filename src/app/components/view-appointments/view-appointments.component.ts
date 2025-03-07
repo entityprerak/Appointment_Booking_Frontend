1d51ac86f7d69acaa2aca0d117b613eb3aabee9c
Syed-Munzer <sdmunzer@gmail.com>
2025-03-07 15:16:54 +0530

    pehla khumar

---
+++ src/app/components/view-appointments/view-appointments.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-appointments',
+  imports: [],
+  templateUrl: './view-appointments.component.html',
+  styleUrl: './view-appointments.component.css'
+})
+export class ViewAppointmentsComponent {
+
+}
